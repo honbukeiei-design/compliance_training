712873bb632ee4f31a7ecd3600e455a07660510e
honbukeiei-design <honbu.keiei@gmail.com>
2026-06-02 13:47:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.set_page_config(page_title="コンプライアンス研修", page_icon="🎧", layout="wide")
 st.title("コンプライアンス研修")
-st.link_button("研修を開く","https://honbukeiei-design.github.io/vr_compliance20260602/")
+st.link_button("研修を開く","https://honbukeiei-design.github.io/compliance_training/")
