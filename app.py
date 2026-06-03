c221fdf2bddaf6476509d53471c1669eef91aac4
honbukeiei-design <honbu.keiei@gmail.com>
2026-06-03 11:53:26 +0900

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="コンプライアンス研修", page_icon="🎧", layout="wide")
-st.title("コンプライアンス研修 10シナリオ版")
-st.write("GitHub Pages公開時は index.html を開いてください。Streamlitで案内ページとして使う場合は、このファイルを利用できます。")
-st.link_button("研修を開く", "./index.html")
