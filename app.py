938807f958f4582a453f744d350b15212aebbdd2
honbukeiei-design <honbu.keiei@gmail.com>
2026-06-03 10:50:39 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+
 st.set_page_config(page_title="コンプライアンス研修", page_icon="🎧", layout="wide")
-st.title("コンプライアンス研修")
-st.link_button("研修を開く","https://honbukeiei-design.github.io/vr_compliance20260602/")
+st.title("コンプライアンス研修 10シナリオ版")
+st.write("GitHub Pages公開時は index.html を開いてください。Streamlitで案内ページとして使う場合は、このファイルを利用できます。")
+st.link_button("研修を開く", "./index.html")
